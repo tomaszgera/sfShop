c91b373a788c178690a2186e9ea15fb4e54b9c5a
tony <tony@thinkpad.dom>
2015-03-22 14:50:30 +0100

    zmiana query w kontrolerze kategorii

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -4,19 +4,32 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class CategoryController extends Controller
 {
     /**
-     * @Route("/list", name="category")
+     * @Route("/list")
      *
      */
     public function listAction()
     {
-        $categories = $this->getDoctrine()
-            ->getRepository('AppBundle:Category')
-            ->findAll();
+        $qb = $this
+                ->getDoctrine()
+                ->getManager()
+                ->createQueryBuilder();
+        
+        $qb
+                ->select('c, p')
+                ->from('AppBundle:Category', 'c')
+                ->innerJoin('c.products', 'p');
+        
+        $categories = $qb
+                ->getQuery()
+                ->getResult();
+        
+//        $categories = $this->getDoctrine()
+//            ->getRepository('AppBundle:Category')
+//            ->findAll();
 
         return $this->render('Category/list.html.twig', [
             'categories' => $categories
